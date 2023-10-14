dd1485061b7b2b25b40cd41b894a9fd8d71874e9
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-14 18:57:46 -0400

    developed basic features

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,73 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TaskList from './components/TaskList';
+import { Task } from './interfaces/task';
+import TaskForm, { FormFields } from './components/TaskForm';
+import { useEffect, useState } from 'react';
+
+const isLocalStorageAvailable = (): boolean => {
+	const test = 'test' + new Date().toISOString();
+	try {
+		window.localStorage.setItem(test, test);
+		window.localStorage.removeItem(test);
+		return true;
+	} catch (e) {
+		return false;
+	}
+};
+
+const getStoredTasks = (): Task[] => {
+	if (!isLocalStorageAvailable()) return [];
+	const storedTasks = window.localStorage.getItem('tasks');
+	if (!storedTasks) return [];
+	return JSON.parse(storedTasks);
+};
 
 function App() {
-	return <>App</>;
+	const [tasks, setTasks] = useState<Task[]>(getStoredTasks);
+
+	useEffect(() => {
+		if (!isLocalStorageAvailable()) return;
+		window.localStorage.setItem('tasks', JSON.stringify(tasks));
+	}, [tasks]);
+
+	const addTask = ({ title, category, dueTime }: FormFields) => {
+		const id = (tasks.slice(-1)[0]?.id ?? 0) + 1;
+
+		setTasks([
+			...tasks,
+			{ id, title, category, dueTime: new Date(dueTime) },
+		]);
+	};
+
+	const completeTask = (id: number) => {
+		const idx = tasks.findIndex((task) => task.id === id);
+		if (idx < 0) return;
+
+		const newTasks = [...tasks];
+		newTasks.splice(idx, 1);
+		setTasks(newTasks);
+	};
+
+	const deleteTask = (id: number) => {
+		const idx = tasks.findIndex((task) => task.id === id);
+		if (idx < 0) return;
+
+		const newTasks = [...tasks];
+		newTasks.splice(idx, 1);
+		setTasks(newTasks);
+	};
+
+	return (
+		<>
+			<TaskForm addTask={addTask} />
+			<TaskList
+				tasks={tasks}
+				completeTask={completeTask}
+				deleteTask={deleteTask}
+			/>
+		</>
+	);
 }
 
 export default App;
