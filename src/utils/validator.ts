024600c73594ac3c140d4e54332d037782e2c6ff
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-14 18:56:52 -0400

    added form validator

---
+++ src/utils/validator.ts
@@ -0,0 +1,19 @@
+import * as yup from 'yup';
+import categories from '../constants/categories';
+
+const validationSchema = yup.object({
+	title: yup
+		.string()
+		.min(1, 'Title must not be empty')
+		.required('Title is required'),
+	category: yup
+		.string()
+		.oneOf(categories, `Category must be one of ${categories.join(', ')}`)
+		.required('Category is required'),
+	dueTime: yup
+		.date()
+		.min(new Date(), 'Due time must not be in the past')
+		.required('Due time is required'),
+});
+
+export default validationSchema;
